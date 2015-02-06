72706fea2145a79053f6db9851d9ccd0df75b392
tgibbons-css <tgibbons@css.edu>
2015-02-05 21:45:44 -0600

    Initial commit

---
+++ app/src/main/java/edu/css/cis3334/eventlisteningsamples/MainActivity.java
@@ -0,0 +1,27 @@
+package edu.css.cis3334.eventlisteningsamples;
+
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.EditText;
+
+
+public class MainActivity extends ActionBarActivity {
+
+    EditText text1;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        text1 = (EditText) findViewById(R.id.editText1);
+
+
+    }
+
+
+
+
+}
